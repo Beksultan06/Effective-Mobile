c79f9fc04691fb46cec25b5e85e205812b6a0627
didiotghima <nurlanuuulubeksultan@gmail.com>
2025-01-16 01:20:01 +0600

    add readme.md

--- core/settings.py
+++ core/settings.py
@@ -185,17 +185,5 @@ JAZZMIN_UI_TWEAKS = {
     "actions_sticky_top": False
 }
 
-# ckeditor
-CKEDITOR_UPLOAD_PATH = "uploads/"
-CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'  # URL to jQuery
-CKEDITOR_IMAGE_BACKEND = "pillow"  # Путь к пакету Pillow для обработки изображений
-CKEDITOR_CONFIGS = {
-    'default': {
-        'toolbar': 'Custom',  # Вы можете настроить свою собственную панель инструментов CKEditor
-        'height': 300,
-        'width': 800,
-    },
-}
-
 CSRF_COOKIE_SECURE = False  # Для локальной разработки
 CSRF_COOKIE_HTTPONLY = True
